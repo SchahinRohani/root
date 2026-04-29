b002b27dc2a2122641957bdc687b9ccb7f9c69c5
Schahin <schahin@rouhanizadeh.de>
2026-04-29 18:36:22 +0200

    chore: enable pre-commit hooks via nix flake (#2)

--- flake.nix
+++ flake.nix
@@ -9,7 +9,12 @@
     };
   };
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    self,
+    nixpkgs,
+    pre-commit-hooks,
+    ...
+  }: let
     systems = [
       "x86_64-linux"
       "aarch64-linux"
@@ -23,7 +28,14 @@
         inherit system;
         config = {};
       };
+      config = {
+        pre-commit = pre-commit-hooks.lib.${system}.run {
+          src = self;
+          hooks = import ./tools/pre-commit.nix {inherit pkgs;};
+        };
+      };
     in rec {
+      checks.pre-commit = config.pre-commit;
       formatter = pkgs.alejandra;
 
       devShells.default = pkgs.mkShell {
@@ -33,8 +45,12 @@
           tokei
           ripgrep
           gh
+          pre-commit
         ];
         shellHook = ''
+          # Generate the .pre-commit-config.yaml symlink when entering the dev shell
+          ${config.pre-commit.shellHook}
+
           echo "Welcome to root dev shell on ${system}!"
         '';
       };
@@ -42,5 +58,6 @@
   in {
     devShells = forEachSystem (system: (createSpace system).devShells);
     formatter = forEachSystem (system: (createSpace system).formatter);
+    checks = forEachSystem (system: (createSpace system).checks);
   };
 }
